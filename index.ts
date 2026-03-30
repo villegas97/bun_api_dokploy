66a067d141161084a90a3218f2453f7953540cdf
Luis Villegas <lualviva97@gmail.com>
2026-03-29 22:30:33 -0600

    Hostname added to provide access

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { UserController } from "./src/controllers/user.controller";
 
 const server = Bun.serve({
   port: 3001,
+  hostname: "0.0.0.0",
   async fetch(req) {
     const url = new URL(req.url);
 
